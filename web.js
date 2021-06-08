58091f49b3cf7de7d719f178214d48953cc90d05
tomas berhane <tomib@tomass-MacBook-Air.local>
2021-06-08 16:41:29 +0200

    solved  all selector exercise

---
+++ web.js
@@ -0,0 +1,86 @@
+/*
+        JS Exercises
+        
+        
+        
+       
+        
+        
+        EX17) Write a function to trim just the first 50 characters in the first paragraph for each blog post
+        EX18) Write a function and attach it to the "Newer" button, to add new Blog Post (just div and title)
+        EX19) Write a function and attach it to the "Older" button, to remove the last Blog Post
+        EX20) Write an alert with the name of the author every time the user hover with the mouse over an author name
+    */
+
+
+        window.onload = function() {
+         
+        
+
+    // EX11) Write a function to add a new link into the navbar
+    // function navbarLink(){
+    //     let links = document.querySelector('nav')
+    
+    //     let a = document.createElement('a')
+
+    //     a.innerHTML = 'new link here'
+    //     a.href = "#"
+    //     links.appendChild(a)
+        
+    
+    // }
+    // navbarLink()
+   
+
+
+
+
+    // EX12) Write a function to change the color of the main title
+    // function MainTitle(){
+    //     let title = document.querySelector('div.jumbotron > div > h1')
+    //     title.style.color = 'yellow'
+    // }
+    // MainTitle()
+
+
+
+
+    // EX13) Write a function to change the background of the jumbotron
+
+    // function jumbotronColor(){
+    //     let jumbotron = document.querySelector('.jumbotron')
+    //     jumbotron.style.backgroundColor = 'yellow'
+    // }
+    // jumbotronColor()
+
+
+    // EX14) Write a function to remove all the links under "Elsewhere"
+
+    // function removeLinks(){
+    //     let allLinks = document.querySelector('aside > div:nth-child(3) > ol')
+    //     allLinks.remove()
+    // }
+    // removeLinks()
+
+
+
+
+
+    // EX15) Write a function to change the column size for heading in jumbotron           idk
+    // function JumbotronColumnSize(){
+    //     let jumbotronColumn = document.querySelectorAll('div.jumbotron > div > h1')
+    //     jumbotronColumn.style.columnSize = '100px'
+    // }
+    // JumbotronColumnSize()
+
+
+
+
+    // EX16) Write a function to remove the "Search" magnifying glass icon
+//     function removeIcon(){
+//         let icon = document.querySelector('header > div > div > a.text-muted > svg')
+//         icon.remove()
+//     }
+// removeIcon()
+
+}
